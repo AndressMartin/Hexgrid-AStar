7d65b1e474471b1d0b68854ee39543e1f8bdcdb3
Andress Martin de Almeida <andress.contato@gmail.com>
2024-05-23 00:57:45 -0300

    Created a way to restart scene

---
+++ Assets/_Project/Scripts/Controls/SceneController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneController : MonoBehaviour
+{
+    [SerializeField] private Button restartSceneButton;
+
+    private void Start()
+    {
+        restartSceneButton.onClick.AddListener(RestartScene);
+    }
+
+    public void RestartScene()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+}
